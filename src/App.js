8032ade3c007caa44853bc64a32eb75d6492b4ec
kolumr <kolumronni@gmail.com>
2021-02-16 14:46:24 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
+import { FaQuoteRight } from 'react-icons/fa';
+import data from './data';
+function App() {
+  const [people,setPeople] = useState(data);
+  const [index, setIndex] = useState(0);
+  return <h2>slider project setup</h2>;
+}
+
+export default App;
